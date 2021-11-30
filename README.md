2e0c62ddca9b2eb12efd2ed5d4604b01610ca771
david.coelho <david@coelho.info>
2021-11-30 03:43:12 +0000

    README and docs

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # Dun Bradstreet
+
+## Instructions
+- Implement the below assignment and should be runnable without compilation errors / warnings
+- Submit a production ready code with all what that means to you
+- Any programming language can be used (preferably Core Java / MapReduce)
+- Must use a design pattern
+- Add necessary doc
+- Add Junit test cases
+- Maven / Gradle should be fine
+- Assignment Code can be uploaded in GitHub / Gitlab
+- Must have a readme doc that explains the steps to build and run
+
+## How to's
+
+### Testing
+```bash
+./gradlew clean test
+```
+
+### Running the application
+```bash
+./gradlew bootRun
+```
+
+### Executing load test
+```bash
+cd gatling-load-test
+./gradlew clean gatlingRun
+```
+
+## Design decisions
+
+> In order to keep the code as simple, I decided to not use many frameworks 
+unless it was justified to address any of the requirements.
+
+> The code has 100% coverage and the volume requirements (1M) was achieved, as you can see int he Gatling report.
+
+> Most of the tests is API tests which covers all scenarios. 
+> Initially I implemented few unit tests, but I removed them when they became duplicated. 
+
+> The part of the code responsible by writing json in the file was implemented using Spring @Async
+> I decided to make it asynchronous as it is maybe the main bottle neck this application can have
+
+## Metrics
+
+### Code Coverage
+![code coverage][doc/code-coverage.png]
+
+### Gatling Load Test
+Gatling metrics: ![metrics-loadtest](doc/metrics-loadtest/index.html)
+```
+================================================================================
+---- Global Information --------------------------------------------------------
+> request count                                    1050000 (OK=1050000 KO=0    )
+> min response time                                      0 (OK=0      KO=-     )
+> max response time                                    487 (OK=487    KO=-     )
+> mean response time                                     3 (OK=3      KO=-     )
+> std deviation                                          6 (OK=6      KO=-     )
+> response time 50th percentile                          2 (OK=2      KO=-     )
+> response time 75th percentile                          2 (OK=2      KO=-     )
+> response time 95th percentile                          7 (OK=7      KO=-     )
+> response time 99th percentile                         26 (OK=26     KO=-     )
+> mean requests/sec                                3763.441 (OK=3763.441 KO=-  )
+---- Response Time Distribution ------------------------------------------------
+> t < 800 ms                                       1050000 (100%)
+> 800 ms < t < 1200 ms                                   0 (  0%)
+> t > 1200 ms                                            0 (  0%)
+> failed                                                 0 (  0%)
+================================================================================
+```
+
+
---
+++ doc/metrics-loadtest/js/stats.js
@@ -0,0 +1,203 @@
+var stats = {
+    type: "GROUP",
+name: "Global Information",
+path: "",
+pathFormatted: "group_missing-name-b06d1",
+stats: {
+    "name": "Global Information",
+    "numberOfRequests": {
+        "total": "1050000",
+        "ok": "1050000",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "487",
+        "ok": "487",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "3",
+        "ok": "3",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "6",
+        "ok": "6",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "2",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "2",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "7",
+        "ok": "7",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "26",
+        "ok": "26",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "count": 1050000,
+    "percentage": 100
+},
+    "group2": {
+    "name": "800 ms < t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t > 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "count": 0,
+    "percentage": 0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "3763.441",
+        "ok": "3763.441",
+        "ko": "-"
+    }
+},
+contents: {
+"req_addactivity-api-58507": {
+        type: "REQUEST",
+        name: "AddActivity-API",
+path: "AddActivity-API",
+pathFormatted: "req_addactivity-api-58507",
+stats: {
+    "name": "AddActivity-API",
+    "numberOfRequests": {
+        "total": "1050000",
+        "ok": "1050000",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "487",
+        "ok": "487",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "3",
+        "ok": "3",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "6",
+        "ok": "6",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "2",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "2",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "7",
+        "ok": "7",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "26",
+        "ok": "26",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "count": 1050000,
+    "percentage": 100
+},
+    "group2": {
+    "name": "800 ms < t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t > 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "count": 0,
+    "percentage": 0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "3763.441",
+        "ok": "3763.441",
+        "ko": "-"
+    }
+}
+    }
+}
+
+}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#percentiles3").append(stat.percentiles3.total);
+    $("#percentiles3OK").append(stat.percentiles3.ok);
+    $("#percentiles3KO").append(stat.percentiles3.ko);
+
+    $("#percentiles4").append(stat.percentiles4.total);
+    $("#percentiles4OK").append(stat.percentiles4.ok);
+    $("#percentiles4KO").append(stat.percentiles4.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
